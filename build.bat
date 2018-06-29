90765396a1e6ec2813c91d51d82f90238cee7b01
Elvis Ayiemba <ayiembaelvis@gmail.com>
2018-06-29 17:26:32 +0300

    added build.bat for myget

---
+++ build.bat
@@ -0,0 +1,23 @@
+﻿@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Debug
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MpesaLibrary.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+
+%nuget% pack "src\MpesaLib.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
