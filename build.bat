ad9526cfb97ca26a95732bac1e3117221db26b1d
Elvis Ayiemba <ayiembaelvis@gmail.com>
2018-07-02 17:48:05 +0300

    Moved .nuspec file

--- build.bat
+++ build.bat
@@ -20,4 +20,4 @@ mkdir Build
 mkdir Build\lib
 mkdir Build\lib\netstandard2.0
 
-%nuget% pack "src\MpesaLib\MpesaLib.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\MpesaLib.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
