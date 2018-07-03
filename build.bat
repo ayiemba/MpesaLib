c6ec9101324dc8b1228618ba8a33c34bd51c5038
Elvis Ayiemba <ayiembaelvis@gmail.com>
2018-07-03 12:46:38 +0300

    test

--- build.bat
+++ build.bat
@@ -17,11 +17,11 @@ if "%nuget%" == "" (
 Set msBuildLocation="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
 
 
-%msBuildLocation% MpesaLibrary.sln /t:restore /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%msBuildLocation% MpesaLibrary.sln /t:restore /t:pack /p:IncludeSymbols=true /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\netstandard2.0
 
 
-%nuget% pack "src\MpesaLib.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
