5b2379aa9cf7700464016f13e0c7796f3e4a8051
Elvis Ayiemba <ayiembaelvis@gmail.com>
2018-07-30 09:13:35 +0300

    test

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-Set msBuildLocation="C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\msbuild.exe"
+Set msBuildLocation="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
 
 
 %msBuildLocation% MpesaLibrary.sln /t:restore /t:pack /p:IncludeSymbols=true /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
