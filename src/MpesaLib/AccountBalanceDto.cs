3fdfdfc2a0e594d116f0fc654f10686a3820da1e
elvis ayiemba <ayiembaelvis@gmail.com>
2018-12-01 14:02:37 +0300

    renamed methods and parameters

--- src/MpesaLib/AccountBalanceDto.cs
+++ src/MpesaLib/AccountBalanceDto.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MpesaLib.Models
+namespace MpesaLib
 {
-    public class BusinessToBusiness
+    public class AccountBalanceDto
     {
-        public string Initiator { get; set; } 
+        public string Initiator { get; set; }
         public string SecurityCredential { get; set; }
-        public string CommandID { get; set; }
-        public string SenderIdentifierType { get; set; }
-        public string RecieverIdentifierType { get; set; }
+        public string CommandID { get; } = "AccountBalance";
         public string Amount { get; set; }
         public string PartyA { get; set; }
-        public string PartyB { get; set; }
-        public string AccountReference { get; set; }
+        public string IdentifierType { get; set; }
         public string Remarks { get; set; }
         public string QueueTimeOutURL { get; set; }
         public string ResultURL { get; set; }
