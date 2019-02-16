c8bb3cb2aaec5367cc326b02de4cdd01c64a859e
Elvis Ayiemba <ayiembaelvis@gmail.com>
2019-02-16 20:00:35 +0300

    Adding test project

---
+++ MpesaLib.Tests/LipaNaMpesaOnlineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MpesaLib.Tests
+{
+    public class LipaNaMpesaOnlineTest
+    {
+        [Fact]
+        public void ShouldReturnAnAuthToken()
+        {
+
+        }
+    }
+}
